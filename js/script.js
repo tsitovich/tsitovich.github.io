c722a7c654c651c5f04b840809b6cdf78e3f0309
tsitovich <tzitovich@mail.ru>
2023-09-14 19:34:11 +0500

    1409

--- js/script.js
+++ js/script.js
@@ -11,53 +11,11 @@ ItcSlider.getOrCreateInstance('.itc-slider', {
 
 
 
-document.addEventListener('DOMContentLoaded', function () {
+ItcSubmitForm.getOrCreateInstance('form');
 
-    $('.contacts__form').validate({
-        rules: {
-            name: {
-                required: true,
-                minlength: 5
-            },
-            email: {
-                required: true,
-                email: true
-            }, 
-            checkbox: {
-                required: true
-            }
-        },
-        messages: {
-            name: {
-                required: "Пожалуйста, введите своё имя",
-                minlength: jQuery.validator.format("Не менее {0} символов!")
-            },
-            email: {
-                required: "Пожалуйста, введите свою почту",
-                email: "Неправильно введён адрес почты"
-            },
-            checkbox: {
-                required: "Поставьте галочку"
-            }
-        }
-    });
-
-
-    $('form').submit(function (e) {
-        e.preventDefault();
-
-        if (!$(this).valid()) {
-            return;
-        }
-
-        $.ajax({
-            type: "POST",
-            url: "mailer/smart.php",
-            data: $(this).serialize()
-        }).done(function () {
-            $(this).find("input").val("");
-            $('form').trigger('reset');
-        });
-        return false;
-    });
+ItcSubmitForm.getOrCreateInstance('form', {
+    isCheckValidationOnClient: false, // проверять форму перед отправкой на сервер
+    attachMaxItems: 5, // максимальное количество файлов, которые можно добавить к форме
+    attachMaxFileSize: 512, // 512 Кбайт - максимальный размер файла
+    attachExt: ['jpg', 'jpeg', 'bmp', 'gif', 'png'] // допустимые расширения файлов
 });
